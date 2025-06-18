bcd9650f25c3d05d731ce1ae9ea696c66ab84725
kumar866 <kumarborelli0@gmail.com>
2025-06-18 20:43:44 +0530

    update

--- Server/controllers/webhooks.js
+++ Server/controllers/webhooks.js
@@ -1,65 +1,125 @@
-import { Webhook } from 'svix';
-import User from '../model/User.js';
-import dotenv from 'dotenv';
-dotenv.config();
+import { Webhook } from "svix";
+import User from "../models/User.js";
+import Stripe from "stripe";
+import { Purchase } from "../models/Purchase.js";
+import Course from "../models/Course.js";
+
+// API Controller Function to Manage Clerk User with database
 export const clerkWebhooks = async (req, res) => {
   try {
-    const wbhook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
-    const payload = req.body; 
-    await wbhook.verify(JSON.stringify(req.body), {
-      'svix-id': req.headers['svix-id'],
-      'svix-timestamp': req.headers['svix-timestamp'],
-      'svix-signature': req.headers['svix-signature'],
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+    await whook.verify(JSON.stringify(req.body), {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
     });
 
     const { data, type } = req.body;
-    console.log(` Clerk webhook received: ${type}`);
 
     switch (type) {
-      case 'user.created': {
+      case "user.created": {
         const userData = {
           _id: data.id,
           email: data.email_addresses[0].email_address,
-          name: data.first_name + "  " + data.last_name,
+          name: data.first_name + " " + data.last_name,
           imageUrl: data.image_url,
         };
-
         await User.create(userData);
-        return res.status(201).json({
-          message: 'User created successfully',
-          data: userData,
-        })
-        
+        res.json({});
+        break;
       }
 
-      case 'user.updated': {
+      case "user.updated": {
         const userData = {
-          email: data.email_addresses?.[0]?.email_address,
-          name: data.first_name + "  " + data.last_name,
+          email: data.email_address[0].email_address,
+          name: data.first_name + " " + data.last_name,
           imageUrl: data.image_url,
         };
-
         await User.findByIdAndUpdate(data.id, userData);
-        return res.status(200).json({
-          message: 'User updated successfully',
-          data: userData,
-        });
-        break
+        res.json({});
+        break;
       }
 
-      case 'user.deleted': {
+      case "user.deleted": {
         await User.findByIdAndDelete(data.id);
-        return res.status(200).json({
-          message: 'User deleted successfully',
-        });
-        break
+        res.json({});
+        break;
       }
 
       default:
-        return res.status(400).json({ message: 'Unhandled event type' });
+        break;
     }
   } catch (error) {
-    console.error('Error handling Clerk webhook:', error);
-    return res.status(500).json({ message: 'Internal server error' });
+    res.json({ success: false, message: error.message });
   }
-}
+};
+
+const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY);
+export const stripeWebhooks = async (request, response) => {
+  const sig = request.headers["stripe-signature"];
+
+  let event;
+
+  try {
+    event = Stripe.webhooks.constructEvent(
+      request.body,
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  // Handle stripe webhooks
+  switch (event.type) {
+    case "payment_intent.succeeded": {
+      const paymentIntent = event.data.object;
+      const paymentIntentId = paymentIntent.id;
+
+      const session = await stripeInstance.checkout.sessions.list({
+        payment_intent: paymentIntentId,
+      });
+
+      const { purchaseId } = session.data[0].metadata;
+
+      const purchaseData = await Purchase.findById(purchaseId);
+      const userData = await User.findById(purchaseData.userId);
+      const courseData = await Course.findById(
+        purchaseData.courseId.toString()
+      );
+
+      courseData.enrolledStudents.push(userData);
+      await courseData.save();
+
+      userData.enrolledCourses.push(courseData._id);
+      await userData.save();
+
+      purchaseData.status = "completed";
+      await purchaseData.save();
+
+      break;
+    }
+    case "payment_intent.payment_failed": {
+      const paymentIntent = event.data.object;
+      const paymentIntentId = paymentIntent.id;
+
+      const session = await stripeInstance.checkout.sessions.list({
+        payment_intent: paymentIntentId,
+      });
+
+      const { purchaseId } = session.data[0].metadata;
+      const purchaseData = await Purchase.findById(purchaseId);
+      purchaseData.status = "failed";
+      await purchaseData.save();
+
+      break;
+    }
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a response to acknowledge receipt of the event
+  response.json({ received: true });
+};
--- Server/server.js
+++ Server/server.js
@@ -1,30 +1,37 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import "dotenv/config";
-import connectDb from './config/dbconnection.js';
-import  {clerkWebhooks }from './controllers/webhooks.js';
-import bodyParser from 'body-parser';
-const Port= process.env.PORT || 8000;
+import connectDB from "./configs/mongodb.js";
+import { clerkWebhooks, stripeWebhooks } from "./controllers/webhooks.js";
+import educatorRouter from "./routes/educatorRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
+import connectCloudinary from "./configs/cloudinary.js";
+import courseRouter from "./routes/courseRoute.js";
+import userRouter from "./routes/userRoutes.js";
 
-//initializing express app
-const app= express();
-//middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: ['Content-Type', 'Authorization', 'svix-id', 'svix-timestamp', 'svix-signature'],
-    preflightContinue: false,
-}));
-//routes
-app.use("/",(req,res)=>{
-    res.send("Welcome to the API");
-})
-app.post("/clerk",express.json(),clerkWebhooks)
-app.listen(Port,async()=>{
-    console.log(`Server is running on port ${Port}`);
-   await connectDb();
+// Initialize Express
+const app = express();
+
+// Connect to the MongoDB database
+await connectDB();
+// Connect to Cloudinary
+await connectCloudinary();
+
+// Middlewares
+app.use(cors());
+app.use(clerkMiddleware());
+
+// Routes
+app.get("/", (req, res) => res.send("API Working"));
+app.post("/clerk", express.json(), clerkWebhooks);
+app.use("/api/educator", express.json(), educatorRouter);
+app.use("/api/course", express.json(), courseRouter);
+app.use("/api/user", express.json(), userRouter);
+app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
+
+// Port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-    
